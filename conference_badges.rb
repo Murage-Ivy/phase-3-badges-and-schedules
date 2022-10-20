20d754395fb7495d20c47ec4263f116371f118ad
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-20 12:28:25 +0300

    Completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(speaker_name)
+  "Hello, my name is #{speaker_name}."
+
+end
+
+def batch_badge_creator(speakers_list)
+  speakers_list.map { |speaker|
+    badge_maker(speaker)
+  }
+
+end
+
+def assign_rooms(speakers_list)
+  speakers_list.map.with_index(1) { |speaker, room_number|
+    "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
+  }
+
+end
+
+def printer (attendees)
+    batch_badge_creator(attendees).each do |badge_message|
+        puts badge_message
+    end
+    
+    assign_rooms(attendees).each do |assigned_room_number|
+        puts assigned_room_number
+    end 
+
+end 
+        
